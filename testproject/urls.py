9fea69e4a56ae1141dd2b62cd458281a76d0e9c8
Oooom <omishah999@gmail.com>
2020-08-06 20:25:52 +0530

    added url dummies

--- testproject/urls.py
+++ testproject/urls.py
@@ -16,14 +16,77 @@ Including another URLconf
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.conf.urls import url, include
+from django.http import QueryDict
+import json
 
 
 @api_view(['GET','POST'])
 def hello_world(request):
-        if request.method == 'POST':
-                return Response({"message": "Got some data!", "data": request.data})
-        return Response({"message": "Hello, world!"})
+    if request.method == 'POST':
+        return Response({"message": "Got some data!", "data": request.data})
+    return Response({"message": "Hello, world!"})
+
+@api_view(['POST'])
+def user_create(request):
+    request.POST.get("user_type")
+    request.POST.get("username")
+    request.POST.get("password")
+
+    print(request.POST)
+
+    return Response({"status": "User Created Successfully",})
+
+@api_view(['POST', 'GET'])
+def order_create_and_get_details(request):
+    if request.method == 'POST':
+        request.POST.get("client_id")
+        request.POST.get("pick_up_address")
+        request.POST.get("drop_address")
+        request.POST.get("item_description")
+
+        print(request.POST)
+
+        return Response({"status": "Order Created Successfully",})
+    elif request.method == 'GET':
+        print(request.GET)
+
+        return Response({
+            "order_id": request.GET.get('order_id', None),
+            "client_id": 1,
+            "partner_id": None,
+            "pick_up_address": "ABCD",
+            "drop_address": "ABCD",
+            "item_description": "Some description of items",
+            "state": "created",
+        })
+
+@api_view(['PUT'])
+def change_order_state(request, order_id):
+    print(order_id)
+    print(json.loads(request.body))
+    return Response({"status": "Order State updated"})
+
+@api_view(['GET'])
+def partner_details(request):
+    request.GET.get('partner_id', None)
+
+    print(request.GET)
+
+    return Response([{
+        "order_id": request.GET.get('partner_id', None),
+        "client_id": 1,
+        "partner_id": None,
+        "pick_up_address": "ABCD",
+        "drop_address": "ABCD",
+        "item_description": "Some description of items",
+        "state": "created",
+    }])
+
 
 urlpatterns = [
-  url(r'^hello/$', hello_world),
+    url(r'^hello/$', hello_world),
+    url(r'^users/$', user_create),
+    url(r'^orders/$', order_create_and_get_details),
+    url(r'^orders/(?P<order_id>[0-9]+)/$', change_order_state),
+    url(r'^partner/orders/$', partner_details),
 ]
