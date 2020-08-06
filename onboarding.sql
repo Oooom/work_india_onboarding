fd3fd20cd07b44ff378f4829b10c7fd978426b57
Oooom <omishah999@gmail.com>
2020-08-06 23:50:41 +0530

    created and merged db

---
+++ onboarding.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 06, 2020 at 08:18 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `onboarding`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `partner_id` int(11) DEFAULT NULL,
+  `pickup_address` varchar(500) NOT NULL,
+  `drop_address` varchar(500) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'created',
+  `item_description` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(20) NOT NULL,
+  `password` blob NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `foreign key 1` (`partner_id`),
+  ADD KEY `foreign key 2` (`client_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`) USING BTREE,
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `foreign key 1` FOREIGN KEY (`partner_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `foreign key 2` FOREIGN KEY (`client_id`) REFERENCES `users` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- testproject/urls.py
+++ testproject/urls.py
@@ -13,80 +13,155 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from django.conf.urls import url, include
-from django.http import QueryDict
+from   rest_framework.response   import Response
+from   rest_framework.decorators import api_view
+from   django.conf.urls          import url, include
+from   django.http               import QueryDict
 import json
+import pymysql
+import hashlib
+
+conn = pymysql.connect(host="127.0.0.1",
+                       user="root",
+                       password="root",
+                       db="onboarding")
+cursor = conn.cursor()
 
 
-@api_view(['GET','POST'])
+@api_view(['GET', 'POST'])
 def hello_world(request):
     if request.method == 'POST':
         return Response({"message": "Got some data!", "data": request.data})
     return Response({"message": "Hello, world!"})
 
+
 @api_view(['POST'])
 def user_create(request):
-    request.POST.get("user_type")
-    request.POST.get("username")
-    request.POST.get("password")
+    username        = request.POST.get("username")
+    password_hashed = hashlib.md5(str.encode( request.POST.get("password") )).digest()
+    user_type       = request.POST.get("user_type")
 
-    print(request.POST)
+    try:
+        cursor.execute( "INSERT INTO users(username, password, type) VALUES('" + username + "',%s, '" + user_type + "')", (password_hashed))
+        conn.commit()
 
-    return Response({"status": "User Created Successfully",})
+        return Response({
+            "success" : True,
+            "status"  : "User Created Successfully",
+        })
+    except pymysql.Error as e:
+        return Response({
+            "success" : False,
+            "error"   : "%d: %s" % (e.args[0], e.args[1])
+        })
+
 
 @api_view(['POST', 'GET'])
 def order_create_and_get_details(request):
     if request.method == 'POST':
-        request.POST.get("client_id")
-        request.POST.get("pick_up_address")
-        request.POST.get("drop_address")
-        request.POST.get("item_description")
+        client_id        = int(request.POST.get("client_id"))
+        pickup_address   = request.POST.get("pick_up_address")
+        drop_address     = request.POST.get("drop_address")
+        item_description = request.POST.get("item_description")
 
-        print(request.POST)
+        try:
+            cursor.execute( "INSERT INTO orders(client_id, pickup_address, drop_address, item_description, partner_id) VALUES(" + str(client_id) + ",'" + pickup_address + "', '" + drop_address + "', '" + item_description + "', NULL)")
+            conn.commit()
 
-        return Response({"status": "Order Created Successfully",})
+            return Response({
+                "success" : True,
+                "status"  : "Order Created Successfully",
+            })
+        except pymysql.Error as e:
+            return Response({
+                "success" : False,
+                "error"   : "%d: %s" % (e.args[0], e.args[1])
+            })
+
     elif request.method == 'GET':
-        print(request.GET)
+        try:
+            cursor.execute("SELECT * FROM orders WHERE order_id=" +
+                           request.GET.get('order_id', -1))
+            conn.commit()
 
-        return Response({
-            "order_id": request.GET.get('order_id', None),
-            "client_id": 1,
-            "partner_id": None,
-            "pick_up_address": "ABCD",
-            "drop_address": "ABCD",
-            "item_description": "Some description of items",
-            "state": "created",
-        })
+            result = cursor.fetchone()
+
+            if result is None:
+                return Response({
+                    "success" : False,
+                    "error"   : "No such order exists!"
+                })
+
+            return Response({
+                "order_id"         : request.GET.get('order_id', -1),
+                "client_id"        : result[1],
+                "partner_id"       : result[2],
+                "pick_up_address"  : result[3],
+                "drop_address"     : result[4],
+                "item_description" : result[6],
+                "state"            : result[5],
+            })
+        except pymysql.Error as e:
+            return Response({
+                "success" : False,
+                "error"   : "%d: %s" % (e.args[0], e.args[1])
+            })
+
 
 @api_view(['PUT'])
 def change_order_state(request, order_id):
-    print(order_id)
-    print(json.loads(request.body))
-    return Response({"status": "Order State updated"})
+    order_id = int(order_id)
+    newState = json.loads(request.body)['status']
+
+    try:
+        cursor.execute("UPDATE orders SET status='"+newState+"' WHERE order_id="+ str(order_id))
+        conn.commit()
+
+        return Response({
+            "success" : True,
+            "status"  : "Order State updated"
+        })
+    except pymysql.Error as e:
+        return Response({
+            "success" : False,
+            "error"   : "%d: %s" % (e.args[0], e.args[1])
+        })
+
 
 @api_view(['GET'])
 def partner_details(request):
-    request.GET.get('partner_id', None)
+    partner_id = int(request.GET.get('partner_id', None))
 
-    print(request.GET)
+    try:
+        cursor.execute("SELECT * FROM orders WHERE partner_id="+str(partner_id))
+        conn.commit()
 
-    return Response([{
-        "order_id": request.GET.get('partner_id', None),
-        "client_id": 1,
-        "partner_id": None,
-        "pick_up_address": "ABCD",
-        "drop_address": "ABCD",
-        "item_description": "Some description of items",
-        "state": "created",
-    }])
+        all_orders = []
+
+        for order in cursor.fetchall():
+            all_orders.append({
+                "order_id"         : request.GET.get('order_id', -1),
+                "client_id"        : order[1],
+                "partner_id"       : order[2],
+                "pick_up_address"  : order[3],
+                "drop_address"     : order[4],
+                "item_description" : order[6],
+                "state"            : order[5],
+            })
+
+        return Response(all_orders)
+
+    except pymysql.Error as e:
+        return Response({
+            "success" : False,
+            "error"   : "%d: %s" % (e.args[0], e.args[1])
+        })
 
 
 urlpatterns = [
-    url(r'^hello/$', hello_world),
-    url(r'^users/$', user_create),
-    url(r'^orders/$', order_create_and_get_details),
-    url(r'^orders/(?P<order_id>[0-9]+)/$', change_order_state),
-    url(r'^partner/orders/$', partner_details),
+    url(r'^hello/$'                       , hello_world),
+    url(r'^users/$'                       , user_create),
+    url(r'^orders/$'                      , order_create_and_get_details),
+    url(r'^orders/(?P<order_id>[0-9]+)/$' , change_order_state),
+    url(r'^partner/orders/$'              , partner_details),
 ]
